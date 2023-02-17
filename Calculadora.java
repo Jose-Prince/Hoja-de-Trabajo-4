9d36bb1d88d5d975fcdcc6954a87a650e2a6ff5c
Jose-Prince <joseprince148mail.com>
2023-02-16 21:29:40 -0600

    Funciones calculador
    Se crearon las funciones que va a usar la clase calculadora

--- Calculadora.java
+++ Calculadora.java
@@ -1,3 +1,30 @@
 public class Calculadora {
     
+    public Calculadora() {
+
+    }
+
+    public int suma(){
+        return 0;
+    }
+
+    public int resta() {
+        return 0;
+    }
+
+    public int multiplicacion() {
+        return 0;
+    }
+
+    public int division() {
+        return 0;
+    }
+
+    public String getItems() {
+        return "";
+    }
+
+    public boolean isOperator() {
+        return false;
+    }
 }
