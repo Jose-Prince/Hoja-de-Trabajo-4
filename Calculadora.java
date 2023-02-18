1becc869faa06c5079ce7e45221fdc5103a24188
Jose-Prince <joseprince148mail.com>
2023-02-18 15:45:40 -0600

    Infix Convertor function
    Se implementa la función que convierte las expresiones infix a postfix

--- Calculadora.java
+++ Calculadora.java
@@ -1,9 +1,11 @@
+import structure5.AbstractStack;
+
 public class Calculadora {
     
     public static boolean instance_flag = false;
     public static Calculadora miCalculadora;
 
-    Calculadora() {
+    private Calculadora() {
         instance_flag = true;
     }
 
@@ -16,27 +18,81 @@ public class Calculadora {
         }
     }
 
-    public int suma(){
-        return 0;
+    public int suma(int a, int b){
+        return a + b;
     }
 
-    public int resta() {
-        return 0;
+    public int resta(int a, int b) {
+        return a - b;
     }
 
-    public int multiplicacion() {
-        return 0;
+    public int multiplicacion(int a, int b) {
+        return a * b;
     }
 
-    public int division() {
-        return 0;
+    public int division(int a, int b) {
+        return a/b;
     }
 
-    public String getItems() {
-        return "";
+    public boolean isOperator(String item) {
+        boolean operator = false;
+        if (item.matches("[+,-,*,/]"))
+            operator = true;
+        return operator;
     }
 
-    public boolean isOperator() {
-        return false;
+    public String[] infixToPostfix(String[] infix, AbstractStack<String> stack){
+        String[] postfix = new String[infix.length];
+        int i;
+        for (i = 0; i < infix.length; i++){
+
+            if (infix[i].matches("[0-9]*")){
+                postfix[i] = infix[i];
+            } else if (infix[i].matches("[(]")){
+                stack.push(infix[i]);
+            } else if (infix[i].matches("[*,/]")){
+                stack.push(infix[i]);
+            } else if (infix[i].matches("[+,-]")){
+                int signo = 0;
+                for (int u = 0; u < stack.size();u++){
+                    if (infix[i].matches("[*,/]")){
+                        signo++;
+                    } 
+                }
+
+                if (signo > 0){
+                    while (stack.isEmpty() == false){
+                        for (int u = 0; u < stack.size();u++){
+                            String operador = stack.peek();
+                            stack.pop();
+                            postfix[i+u] = operador;                  
+                        }
+                        stack.push(infix[i]);
+                    }
+                } else {
+                    stack.push(infix[i]);
+                }
+            } else if (infix[i].matches("[)]")){
+                while (stack.isEmpty() == false && stack.peek() != "(") {
+                    for (int u = 0; u < stack.size(); u++){
+                        if (stack.peek() == "("){
+                            stack.pop();
+                        } else {
+                            String operador = stack.peek();
+                            stack.pop();
+                            postfix[i+u] = operador;    
+                        }
+                    }   
+                }
+            } 
+        }
+
+        while (stack.isEmpty() == false) {
+            for (int u = 0; u < stack.size(); u++){
+                postfix[i] = stack.peek();
+            }
+        }
+
+        return postfix;
     }
 }
--- CalculadoraPrincipal.java
+++ CalculadoraPrincipal.java
@@ -1,108 +1,128 @@
 import java.io.File;
 import java.util.Scanner;
 
+import structure5.AbstractStack;
+
 public class CalculadoraPrincipal {
     public static <E> void main(String[] args) {
-        System.out.println("Ingrese la ruta del archivo ej C:\\ejemplos\\example1.txt");
         Scanner in = new Scanner(System.in);
-        String fpath = in.nextLine();
-        Calculadora calculadora = new Calculadora();
-        StackFactory<E> factory = new StackFactory<>();
-
         int a;
         int b;
         E resultado;
+        Calculadora calculadora = new Calculadora();
+        StackFactory<E> factory = new StackFactory<>();
+
+        System.out.println("Escoja el tipo de pila que quiera usar: (indique el numero)");
+        System.out.println("\n1. SingledLinkedList\n2. DoublyLinkedList\n3. ArrayLits\n4. Vector\n\nOpción:");
+        try {
+            int opcion = in.nextInt();
+            in.nextLine();
+            
+            AbstractStack<E> stack = factory.getInstance(opcion);
+
+
+            System.out.println("Ingrese la ruta del archivo ej C:\\ejemplos\\example1.txt");
+            
+            String fpath = in.nextLine();
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            in.nextLine();
+            System.out.println("\nOpción inválida");
+        }
+
         
-      try {
-        File myObj = new File(fpath);
-        Scanner myReader = new Scanner(myObj);
-        while (myReader.hasNextLine()) {
-          String data = myReader.nextLine();
-          System.out.println(data);
-          //calculadora.getItems(data);
-          String[] listaStrings = data.split(" ");
-          for (int i = 0; i<listaStrings.length; i++){
-              if (listaStrings[i].matches("[0-9]*")){
-                  factory.getItems(listaStrings[i]);
-                  //listaPostFix = calculadora.getItems(data);
-              } else if (listaStrings[i].matches("[+,-,*,/]")){
-                  if (calculadora.count() > 1){
-                  switch (listaStrings[i]) {
-                      case "+":
-                          b = Integer.valueOf((String) calculadora.peek());
-                          calculadora.pull();
-                          a = Integer.valueOf((String) calculadora.peek());
-                          calculadora.pull();
-                          resultado = (T) String.valueOf(calculadora.suma(a, b));
-                          calculadora.push(resultado);
+    }}
+//       try {
+//         File myObj = new File(fpath);
+//         Scanner myReader = new Scanner(myObj);
+//         while (myReader.hasNextLine()) {
+//           String data = myReader.nextLine();
+//           System.out.println(data);
+//           //calculadora.getItems(data);
+//           String[] listaStrings = data.split(" ");
+//           for (int i = 0; i<listaStrings.length; i++){
+//               if (listaStrings[i].matches("[0-9]*")){
+//                   factory.getItems(listaStrings[i]);
+//                   //listaPostFix = calculadora.getItems(data);
+//               } else if (listaStrings[i].matches("[+,-,*,/]")){
+//                   if (calculadora.count() > 1){
+//                   switch (listaStrings[i]) {
+//                       case "+":
+//                           b = Integer.valueOf((String) calculadora.peek());
+//                           calculadora.pull();
+//                           a = Integer.valueOf((String) calculadora.peek());
+//                           calculadora.pull();
+//                           resultado = (T) String.valueOf(calculadora.suma(a, b));
+//                           calculadora.push(resultado);
   
-                          if (listaStrings[listaStrings.length - 1] == listaStrings[i]){
-                              System.out.println("Resultado: " + resultado);
-                              calculadora.pull();
-                          }
-                          break;
-                      case "-":
-                          b = Integer.valueOf((String) calculadora.peek());
-                          calculadora.pull();
-                          a = Integer.valueOf((String) calculadora.peek());
-                          calculadora.pull();
-                          resultado = (T) String.valueOf(calculadora.resta(a, b));
-                          calculadora.push(resultado);
+//                           if (listaStrings[listaStrings.length - 1] == listaStrings[i]){
+//                               System.out.println("Resultado: " + resultado);
+//                               calculadora.pull();
+//                           }
+//                           break;
+//                       case "-":
+//                           b = Integer.valueOf((String) calculadora.peek());
+//                           calculadora.pull();
+//                           a = Integer.valueOf((String) calculadora.peek());
+//                           calculadora.pull();
+//                           resultado = (T) String.valueOf(calculadora.resta(a, b));
+//                           calculadora.push(resultado);
   
-                          if (listaStrings[listaStrings.length - 1] == listaStrings[i]){
-                              System.out.println("Resultado: " + resultado);
-                              calculadora.pull();
-                          }
-                          break;
-                      case "*":
-                          b = Integer.valueOf((String) calculadora.peek());
-                          calculadora.pull();
-                          a = Integer.valueOf((String) calculadora.peek());
-                          calculadora.pull();
-                          resultado = (T) String.valueOf(calculadora.multiplicacion(a, b));
-                          calculadora.push(resultado);
+//                           if (listaStrings[listaStrings.length - 1] == listaStrings[i]){
+//                               System.out.println("Resultado: " + resultado);
+//                               calculadora.pull();
+//                           }
+//                           break;
+//                       case "*":
+//                           b = Integer.valueOf((String) calculadora.peek());
+//                           calculadora.pull();
+//                           a = Integer.valueOf((String) calculadora.peek());
+//                           calculadora.pull();
+//                           resultado = (T) String.valueOf(calculadora.multiplicacion(a, b));
+//                           calculadora.push(resultado);
   
-                          if (listaStrings[listaStrings.length - 1] == listaStrings[i]){
-                              System.out.println("Resultado: " + resultado);
-                              calculadora.pull();
-                          }
-                          break;
-                      case "/":
-                          b = Integer.valueOf((String) calculadora.peek());
-                          calculadora.pull();
-                          a = Integer.valueOf((String) calculadora.peek());
-                          calculadora.pull();
+//                           if (listaStrings[listaStrings.length - 1] == listaStrings[i]){
+//                               System.out.println("Resultado: " + resultado);
+//                               calculadora.pull();
+//                           }
+//                           break;
+//                       case "/":
+//                           b = Integer.valueOf((String) calculadora.peek());
+//                           calculadora.pull();
+//                           a = Integer.valueOf((String) calculadora.peek());
+//                           calculadora.pull();
                           
-                          if (b == 0){
-                              System.out.println("No se puede realizar la división entre 0");
-                              break;
-                          } else {
-                              resultado = (T) String.valueOf(calculadora.division(a, b));
-                              calculadora.push(resultado);
+//                           if (b == 0){
+//                               System.out.println("No se puede realizar la división entre 0");
+//                               break;
+//                           } else {
+//                               resultado = (T) String.valueOf(calculadora.division(a, b));
+//                               calculadora.push(resultado);
   
-                              if (listaStrings[listaStrings.length - 1] == listaStrings[i]){
-                                  System.out.println("Resultado: " + resultado);
-                                  calculadora.pull();
-                              }
-                          }
+//                               if (listaStrings[listaStrings.length - 1] == listaStrings[i]){
+//                                   System.out.println("Resultado: " + resultado);
+//                                   calculadora.pull();
+//                               }
+//                           }
   
-                          break;
-                      default:
-                          break;
-                      }
-                  } else {
-                      System.out.println("Insuficientes operandos para el operador");
-                  }
-                  }
+//                           break;
+//                       default:
+//                           break;
+//                       }
+//                   } else {
+//                       System.out.println("Insuficientes operandos para el operador");
+//                   }
+//                   }
               
-              }
-          }
+//               }
+//           }
         
-        myReader.close();
-      } catch (FileNotFoundException e) {
-        System.out.println("An error occurred.");
-        e.printStackTrace();
-      }
-    }
-  }
-}
+//         myReader.close();
+//       } catch (FileNotFoundException e) {
+//         System.out.println("An error occurred.");
+//         e.printStackTrace();
+//       }
+//     }
+//   }
+// }
